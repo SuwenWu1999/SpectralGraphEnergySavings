6c219b30eb799989cd338aeca9ec71a18be24b2a
Karissa Stisser <me@example.com>
2021-10-12 12:03:15 -0400

    Adding defs

--- GraphNodes/energyConsumerNode.py
+++ GraphNodes/energyConsumerNode.py
@@ -1,5 +1,7 @@
 import energyNode
+import Misc.defs
 
 class EnergyConsumerNode(energyNode.EnergyNode):
-    def __init__(self):
+    def __init__(self, consumerType):
         print("Making energy source node")
+        self.consumerType = consumerType
---
+++ Misc/defs.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ConsumerType(Enum):
+    CONSTANT = 1
+    MINCHARGE = 2
+    ENDTIME = 3
